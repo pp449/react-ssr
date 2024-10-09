4392a3acf4db2cdbbf300ce86d47f1f373fb62ad
Lee sang Yeop <mma7710@naver.com>
2024-10-09 15:49:07 +0900

    Home -> MoviePage로 파일 명 변경, 헤더 컴포넌트 추가, 영화 데이터를 prop 으로 받도록 변경

---
+++ src/client/components/MoviePage.jsx
@@ -0,0 +1,27 @@
+import MovieItem from './base/MovieItem';
+import React from 'react';
+import Header from './Header';
+
+function MoviePage({ movies }) {
+	return (
+		<>
+			<Header firstMovie={movies[0]} />
+			<div className="container">
+				<main>
+					<section>
+						<h2>지금 인기 있는 영화</h2>
+						<ul className="thumbnail-list">
+							{movies.map(({ id, title, vote_average, poster_path }) => (
+								<li key={id}>
+									<MovieItem rate={vote_average} title={title} thumbnailUrl={poster_path} />
+								</li>
+							))}
+						</ul>
+					</section>
+				</main>
+			</div>
+		</>
+	);
+}
+
+export default MoviePage;
